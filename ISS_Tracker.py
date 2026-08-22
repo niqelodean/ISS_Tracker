c294338f897f545ebd03d2c288e893e85cb34c99
Aniq Fayyadh <aniqfayyadh2007@gmail.com>
2026-08-22 16:27:12 +0800

    Implement location name retrieval from coordinates
    Added a function to retrieve location name from latitude and longitude using OpenStreetMap API.

--- ISS_Tracker.py
+++ ISS_Tracker.py
@@ -16,14 +16,45 @@ def get_iss_location():
 
     return latitude, longitude, timestamp
 
+def  get_location_name (latitude, longitude):
+    url = "https://nominatim.openstreetmap.org/reverse"
+    parameters = {
+        'lat' : latitude,
+        'lon' : longitude,
+        'format' : 'json'
+    }
+    headers = {
+        'User-Agent' : 'ISS-Tracker/1.0'
+    }
+    
+    response = requests.get(url, params=parameters, headers=headers)
+    data = response.json()
+    
+    address = data.get("address", {})
+    
+    location = (
+        address.get("city") or
+        address.get("town") or
+        address.get("village") or
+        address.get("county") or
+        address.get("state") or
+        address.get("country") or
+        "Unknown Location"
+    )
+    
+    return location
+
 def timestamp_to_local_datetime(timestamp):
-    from datetime import datetime, timezone
     local_dt = datetime.fromtimestamp(timestamp, tz=timezone.utc).astimezone() 
     return local_dt.strftime('%b %d, %Y at %I:%M %p')
-
+    
 def send_iss_location_to_discord(latitude, longitude, timestamp):
+    location_name = get_location_name(latitude, longitude)
     message = {
-        "content": f"Current ISS Location:\nLatitude: {latitude}\nLongitude: {longitude}\nTimestamp: {timestamp_to_local_datetime(timestamp)}"
+        "content":  f"Current ISS Location: {location_name}\n"
+                    f'Latitude: {latitude}\n'
+                    f'Longitude: {longitude}\n'
+                    f'Timestamp: {unic_timestamp_to_am_pm_format(timestamp)}'
     }
     requests.post(WEBHOOK_URL, json=message)
 
